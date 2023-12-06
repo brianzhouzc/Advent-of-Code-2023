b6c49c107fe8a4824bebac56496b9f42aa94cc4e
Brian Z <brianzhouzc@gmail.com>
2023-12-06 02:24:16 -0800

    Update day3_part1.py
    Removed unnecessary 2d array

--- day3_part1.py
+++ day3_part1.py
@@ -3,16 +3,8 @@ import re
 f = open('input.txt', 'r')
 lines = f.readlines()
 
-symbol_map = [ [0]*140 for i in range(142)]
-
-line_num = 1
-for line in lines:
-    for m in re.finditer(r'[^.\d\s]', line):
-        symbol_map[line_num][m.start()] = 1
-    line_num = line_num + 1
-
 sum = 0
-line_num = 1
+line_num = 0
 for line in lines:
     for m in re.finditer(r'\d+', line):
         num = int(m.group())
@@ -21,16 +13,18 @@ for line in lines:
 
         valid = False
         # check left on current line
-        if (start > 0 and symbol_map[line_num][start - 1] == 1):
+        if (start > 0 and re.match(r'[^.\d\s]', line[start - 1])):
             valid = True
         # check right on current line
-        elif(start + length < 140 and symbol_map[line_num][start + length] == 1):
+        elif(start + length < 140 and re.match(r'[^.\d\s]', line[start + length])):
             valid = True
         
         for i in range(-1, length + 1):
             if (start + i >= 0 and start + i < 140):
                 # check above and below
-                if(symbol_map[line_num - 1][start + i] == 1 or symbol_map[line_num + 1][start + i] == 1):
+                if (line_num > 0 and re.match(r'[^.\d\s]', lines[line_num - 1][start + i])):
+                    valid = True
+                if(line_num < 139 and re.match(r'[^.\d\s]', lines[line_num + 1][start + i])):
                     valid = True
         
         #print(num, valid)
